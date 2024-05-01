7c6d972b20209eecb73a7e5e92dd5d497f98f978
ValentinAmazing <podrezva@bk.ru>
2024-05-02 05:03:35 +0700

    UIKit Slider Representatioin

---
+++ Guess Postition/SliderRepresentation.swift
@@ -0,0 +1,18 @@
+//
+//  SliderRepresentation.swift
+//  Guess Postition
+//
+//  Created by Valentin on 02.05.2024.
+//
+
+import SwiftUI
+
+struct SliderRepresentation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SliderRepresentation()
+}
