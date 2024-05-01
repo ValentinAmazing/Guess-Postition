a8b25c2bdefa47b1093e5c1b7c227ff4099d4c2e
ValentinAmazing <podrezva@bk.ru>
2024-05-01 17:23:53 +0700

    Initial Commit

---
+++ Guess Postition/Guess_PostitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_PostitionApp.swift
+//  Guess Postition
+//
+//  Created by Valentin on 01.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Guess_PostitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
