a470f3c0fabf47e5beae447ab4f2431c2f6c74f4
ValentinAmazing <podrezva@bk.ru>
2024-05-02 03:08:31 +0700

    game logic

--- Guess Postition/ContentView.swift
+++ Guess Postition/ContentView.swift
@@ -7,15 +7,21 @@
 
 import SwiftUI
 
+let range0_100 = Range(0...100)
+
 struct ContentView: View {
-    @State private var targetValue = 50
-    @State private var currentValue = Double.random(in: 0...100)
+    @State private var targetValue = Int.random(in: range0_100)
+    @State private var currentValue = Int.random(in: range0_100)
+    @State private var score = 0
+    @State private var isShowAlert = false
     
     var body: some View {
         VStack(spacing: 30) {
-            //Slider
             Text("Подвиньте слайдер, как можно ближе к: \(targetValue)")
-            Slider(value: $currentValue) {
+            Slider(
+                value: $currentValue.float(),
+                in: 0...100 //ClosedRange(range0_100, Float.init)
+            ) {
                 Text("lol")
             } minimumValueLabel: {
                 Text("0")
@@ -23,17 +29,43 @@ struct ContentView: View {
                 Text("100")
             }
 
-            Button("Проверь меня", action: {})
+            Button("Проверь меня", action: checkMe)
                 .font(.title)
-            Button("Начать заново", action: {})
+                .alert("Your score", isPresented: $isShowAlert, actions: {}) {
+                    Text("\(score)")
+                }
+            Button("Начать заново", action: startOver)
                 .font(.title)
         }
         .padding()
+    }
+    
+    private func startOver() {
+        targetValue = Int.random(in: range0_100)
+        currentValue = Int.random(in: range0_100)
+        
+    }
+    
+    private func checkMe() {
+        score = computeScore()
+        isShowAlert.toggle()
+    }
+    
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(Double(currentValue)))
+        return 100 - difference
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension Binding where Value == Int {
+    public func float() -> Binding<Float> {
+        return Binding<Float>(get:{ Float(self.wrappedValue) },
+            set: { self.wrappedValue = Int($0)})
     }
 }
